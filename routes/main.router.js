12365208a2fadea14ee0183d5dc2580cf87e5609
sebastienphilippe <45207662+sebastienphilippe@users.noreply.github.com>
2019-03-21 12:21:20 +0100

    Update main.router.js

--- routes/main.router.js
+++ routes/main.router.js
@@ -3,6 +3,7 @@ Imports
 */
     // Nodejs
     const { Router } = require('express');
+    const AuthRouterClass = require('./auth/auth.routes');
 //
 
 /* 
@@ -20,16 +21,20 @@ Definition des router
     // Parent
     const mainRouter = Router();
     const apiRouter = Router();
+
+    // Child
+    const authRouter = new AuthRouterClass();
 //
 
 /* 
 Définition des routes
 */
     mainRouter.use( '/api', apiRouter );
+    apiRouter.use('/auth', authRouter.init());
 //
 
 /* 
 Export
 */
     module.exports = {mainRouter};
-//	
+//
