d4974199796dafbedc1a4403c7bedad675c6ea56
sebastienphilippe <45207662+sebastienphilippe@users.noreply.github.com>
2019-03-21 17:27:56 +0100

    Update category.routes.js

--- routes/category/category.routes.js
+++ routes/category/category.routes.js
@@ -36,20 +36,7 @@ Class definition
         .catch( apiErr => sendApiErrorResponse(res, 'Error during user registration', apiErr));
       })
 
-      // Route USER login
-      CategoryRouter.post('/login', (req, res) => {
-        
-        // Error: no body present
-        if (typeof req.body === 'undefined' || req.body === null) { return sendBodyError(res, 'No body data provided') }
-        // Check fields in the body
-        const { miss, extra, ok } = checkFields(['titre', 'description'], req.body);
-        //=> Error: bad fields
-        if (!ok) { return sendFieldsError(res, 'Bad fields provided', miss, extra) }
-        // Request is OK
-        login(req.body, res)
-        .then( apiRes =>  sendApiSuccessResponse(res, 'User is logged in', apiRes) )
-        .catch( apiErr => sendApiErrorResponse(res, 'Error during user login', apiErr));
-      })
+     
     };
 
     // Initialize routes
